0c6afb6b5d69a52539b84d1c01ca89f01da90202
CXPhoenix <45819421+CXPhoenix@users.noreply.github.com>
2020-12-15 13:57:48 +0800

    Create DemoSubmit.js

---
+++ finnal/DemoSubmit.js
@@ -0,0 +1,27 @@
+// Q1
+document.getElementById("Q").innerHTML = "done";
+
+// Q2
+document.getElementById().innerHTML = "";
+
+// Q3
+var btn = document.getElementById();
+btn.onclick = function() {
+    document.getElementById("Q3text") = "";
+};
+
+// Q4
+var btn = document.getElemnetById("Q4btn");
+btn.onclick = function() {
+    document.getElementById() = "";
+};
+
+// Q5
+var btn = document.getElementById();
+btn.onclick = function() {
+    document.getElementById("").style.color = "d";
+    document.getElementById("Q5te").style.color = "n";
+};
+
+// 加分題
+document.getElementById().style.display = "block";
